aac4194c470c704241b7382d90ec5b4ce7412747
Muhammad Ali <96694357+muhammadallain@users.noreply.github.com>
2023-06-05 20:26:24 +0100

    Update app-setup.js

--- static/app-setup.js
+++ static/app-setup.js
@@ -1,10 +1,10 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyBkxazTEvbyWfEZzkW6HX03Sl0iJNdlQck",
-  authDomain: "divine-tempo-341205.firebaseapp.com",
-  projectId: "divine-tempo-341205",
-  storageBucket: "divine-tempo-341205.appspot.com",
-  messagingSenderId: "1073606828606",
-  appId: "1:1073606828606:web:220fc841232f272481676f"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
